9e97fac3ad7939f64df86cb5f87f04d6ae698d63
Purushothkumar <purushothkumar@hearzap.com>
2024-07-11 09:39:41 +0530

    content menu

--- appsource/SwiftUIConcepts/SwiftUIConcepts/ContextMenu.swift
+++ appsource/SwiftUIConcepts/SwiftUIConcepts/ContextMenu.swift
@@ -9,24 +9,54 @@ import SwiftUI
 
 struct ContextMenu: View {
     @State var backgroundColor = Color.blue
-    var body: some View {
-        VStack(alignment: .leading,spacing: 10.0) {
-            Image(systemName: "house.fill").font(.largeTitle.bold()).foregroundColor(Color.yellow)
-            Text("Name:Purushoth").font(.largeTitle.bold())
-            Text("""
+    @State var address = """
 Address:
 3/622 A Govindan nagar 6th Street
 Palavakkam
 Chennai - 600041
-""").font(.title)
+"""
+    @State var name = "Purushoth"
+
+    var body: some View {
+        VStack(alignment: .leading,spacing: 10.0) {
+            Image(systemName: "house.fill").font(.largeTitle.bold()).foregroundColor(Color.yellow)
+            Text(name).font(.largeTitle.bold())
+            Text(address).font(.title)
 
         }.foregroundColor(.white)
             .padding(30)
             .background(backgroundColor.cornerRadius(30))
             .contextMenu(menuItems: {
-                Text("Menu Item 1")
-                Text("Menu Item 2")
-                Text("Menu Item 3")
+                Button(action: {backgroundColor = .purple}, label: {
+                    Label("Home color", systemImage: "house").font(.largeTitle)
+                })
+                Button(action: {
+                    backgroundColor = .red
+                    name = "PK"
+                }, label: {
+                    Label("Name", systemImage: "person").font(.largeTitle)
+                })
+                Button(action: {
+                    backgroundColor = .green
+                    address = """
+                Address:
+                17 Sardar patel road
+                Kanathur Reddykuppam
+                Chennai - 600041
+                """
+               }, label: {
+                    Label("Address", systemImage: "location").font(.largeTitle)
+                })
+                Button(action: {backgroundColor = Color.blue
+                    address = """
+                Address:
+                3/622 A Govindan nagar 6th Street
+                Palavakkam
+                Chennai - 600041
+                """
+                name = "Purushoth"}, label: {                    Label("revert", systemImage: "arrowshape.turn.up.backward").font(.largeTitle)
+                })
+
             })
     }
 }
