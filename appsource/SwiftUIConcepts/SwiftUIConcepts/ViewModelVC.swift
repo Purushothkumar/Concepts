5fdc1bed02a12018c0604408ae8a8efa15b0536b
Purushothkumar <purushothkumar2710@gmail.com>
2024-07-16 10:21:09 +0530

    ViewModel with Observable Object

--- appsource/SwiftUIConcepts/SwiftUIConcepts/ViewModelVC.swift
+++ appsource/SwiftUIConcepts/SwiftUIConcepts/ViewModelVC.swift
@@ -18,43 +18,63 @@ struct FruitModel:Identifiable{
     }
 }
 
-struct ViewModelVC: View {
-    @State var fruitArray:[FruitModel] = [
+
+class FruitViewModel: ObservableObject  {
+    @Published var fruitArray:[FruitModel] = [
         FruitModel(id: UUID().uuidString, count: 5, name: "APPLE"),
         FruitModel(id: UUID().uuidString, count: 10, name: "BANANA")]
-    var body: some View {
-        NavigationView {
-            List{
-                ForEach(fruitArray) { fruit in                  VStack(alignment: .leading, content: {
-                    HStack{
-                        Text("\(fruit.count)").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/).font(.largeTitle)
-                        
-                        Text(fruit.name).font(.title)
-                    }
-                    Text(fruit.id)
-                    })
-                }.onDelete(perform: deletefruit)
-            }.navigationTitle("FRUIT LIST")
-        }.onAppear{
-            getFruits()
-        }
-    }
+//    @Published var fruitArray:[FruitModel] = []
+    @Published var isLoading: Bool = false
     
     func getFruits(){
         let fruit1 = FruitModel(id: UUID().uuidString, count: 5, name: "orange")
         let fruit2 = FruitModel(id: UUID().uuidString, count: 7, name: "guava")
         let fruit3 = FruitModel(id: UUID().uuidString, count: 8, name: "watermelon")
         let fruit4 = FruitModel(id: UUID().uuidString, count: 21, name: "jack")
-        
-        fruitArray.append(fruit1)
-        fruitArray.append(fruit2)
-        fruitArray.append(fruit3)
-        fruitArray.append(fruit4)
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5){
+            self.fruitArray.append(fruit1)
+            self.fruitArray.append(fruit2)
+            self.fruitArray.append(fruit3)
+            self.fruitArray.append(fruit4)
+            self.isLoading = false
+        }
     }
     func deletefruit(indexSet: IndexSet){
         fruitArray.remove(atOffsets: indexSet)
         
     }
+
+}
+
+struct ViewModelVC: View {
+//    @State var fruitArray:[FruitModel] = [
+//        FruitModel(id: UUID().uuidString, count: 5, name: "APPLE"),
+//        FruitModel(id: UUID().uuidString, count: 10, name: "BANANA")]
+    @ObservedObject var fruitViewModel:FruitViewModel = FruitViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List{
+                if fruitViewModel.isLoading{
+                    ProgressView()
+                }
+                else{
+                ForEach(fruitViewModel.fruitArray) { fruit in                  VStack(alignment: .leading, content: {
+                    HStack{
+                        Text("\(fruit.count)").foregroundColor(.blue).font(.largeTitle)
+                        
+                        Text(fruit.name).font(.title)
+                    }
+                    Text(fruit.id)
+                    })
+                }.onDelete(perform: fruitViewModel.deletefruit)
+            }}.navigationTitle("FRUIT LIST")
+        }.onAppear{
+            fruitViewModel.getFruits()
+        }
+    }
+    
 }
 
 #Preview {
