b33c40e196ae9548e7101e9b571aca8c74505f1b
Purushothkumar <purushothkumar2710@gmail.com>
2024-07-23 20:39:28 +0530

    initializer

--- appsource/SwiftUIConcepts/SwiftUIConcepts/playground/Initializers.playground/Contents.swift
+++ appsource/SwiftUIConcepts/SwiftUIConcepts/playground/Initializers.playground/Contents.swift
@@ -4,12 +4,6 @@ import UIKit
 
 //initializers are special methods used to set up a new instance of a class, structure, or enumeration. They ensure that all properties of the instance are given initial values and perform any other setup or initialization required.
 
-
-
-.
-
-
-
 class Person{
     var name:String
 
@@ -88,12 +82,11 @@ class NewPerson {
 }
 
 
-//MARK:Failable Initializers
-
-//
+//MARK: Failable Initializers
+//It’s sometimes useful to define a class, structure, or enumeration for which initialization can fail. This failure might be triggered by invalid initialization parameter values, the absence of a required external resource, or some other condition that prevents initialization from succeeding.
 
 /*
-
+ Initializers that can return nil if initialization fails.
  */
 
 class FailableInitializers {
@@ -110,9 +103,11 @@ class FailableInitializers {
 }
 
 
-//MARK:Required Initializers
+//MARK: Required Initializers
 
-//
+//Write the required modifier before the definition of a class initializer to indicate that every subclass of the class must implement that initializer:
+
+
 
 /*
  Must be implemented by every subclass.
@@ -127,5 +122,24 @@ class SomeSubclass: RequiredInitializers {
     required init() {
         // Subclass implementation of required initializer
         super.init()
+
     }
 }
+
+// fatalError(super.init()) // fatal error is to be capture for the non UI Components
+// super.init(coder: coder) // for UI Components nscoder
+
+/*
+required init?(coder: NSCoder) {
+       super.init(coder: coder)
+       // Additional setup for SubSafeView
+   }
+   
+   override init(frame: CGRect) {
+       super.init(frame: frame)
+       // Additional setup for SubSafeView
+   }
+*/
+
+
+
