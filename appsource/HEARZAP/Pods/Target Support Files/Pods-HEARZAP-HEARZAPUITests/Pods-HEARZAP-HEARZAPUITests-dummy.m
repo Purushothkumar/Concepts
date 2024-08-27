4dd400a195e5e28e938dfc2fda4f74df1087f3e1
D2E <developer2explore@gmail.com>
2024-08-27 11:55:46 +0530

    project

--- appsource/HEARZAP/Pods/Target Support Files/Pods-HEARZAP-HEARZAPUITests/Pods-HEARZAP-HEARZAPUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HEARZAP_HEARZAPUITests : NSObject
-@end
-@implementation PodsDummy_Pods_HEARZAP_HEARZAPUITests
-@end
--- appsource/HEARZAP/Pods/Target Support Files/Pods-HEARZAP/Pods-HEARZAP-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HEARZAP : NSObject
-@end
-@implementation PodsDummy_Pods_HEARZAP
-@end
